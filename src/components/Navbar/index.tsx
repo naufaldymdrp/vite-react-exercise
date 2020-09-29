def530ecf4b978ce5bc1e547819edadd7ab0a741
naufaldymdrp <naufaldygood@outlook.com>
2020-09-29 11:33:54 +0700

    Add TS plugin for more confenient experience

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,27 @@
 import React, { FC } from "react";
-import "./index.module.css";
+import styles from "./index.module.css";
 
-const Navbar: FC = (props) => <div>fucking hello</div>;
+const Navbar: FC = (props) => {
+  return (
+    <header className={styles.header}>
+      <div>
+        <h1>Logo</h1>
+      </div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Privacy</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Navbar;
